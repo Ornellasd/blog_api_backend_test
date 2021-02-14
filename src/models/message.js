7b92afaebf819659a2df2a7761a57788f66b7c7a
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-02-14 11:01:22 -0800

    change max message character count to 140

--- src/models/message.js
+++ src/models/message.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const MessageSchema = new Schema(
   {
     title: { type: String, required: true, maxLength: 100 },
-    text: { type: String, required: true},
+    text: { type: String, required: true, maxLength: 140},
     user: { type: String, required: true, maxLength: 50 }
   }
 );

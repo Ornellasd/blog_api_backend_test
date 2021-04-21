681a13c1d711fca8ca924497ef700224d93267b0
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-04-21 00:40:22 -0700

    initialize passport.js setup

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
@@ -10,6 +13,39 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(cors());
+
+
+// passport.js stuff
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) { 
+        return done(err);
+      };
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // mongoose setup
 const mongoDB = process.env.DB;
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,9 +4,9 @@ exports.new_user_post = (req, res) => {
   const user = new User({
     username: req.body['signup-username'],
     password: req.body['signup-password']
+  }).save(err => {
+    if(err) {
+      return next(err);
+    };
   });
-
-  console.log(user);
-
-  user.save();
 }
